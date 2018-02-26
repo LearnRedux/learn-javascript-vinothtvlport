e9e24f588fa87123f3b37c7bb1a898706248d0a4
vinothtvlport <36688343+vinothtvlport@users.noreply.github.com>
2018-02-26 09:53:03 -0500

    Javascript Spec file

---
+++ 9.AsyncAwait.spec.js
@@ -0,0 +1,167 @@
+describe('Async/Await', () => {
+
+  const sleep = (ms = 0) => new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+
+  it('by default javascript code is synchronous: it waits nothing', () => {
+    const startTime = Date.now();
+    sleep(100); // 100ms = 0.1 second
+    const stopTime = Date.now();
+
+    // wait is 100 so... 
+    // if it waits it should be at least 100
+    // make your bigger guess
+    expect(0).toBeLessThanOrEqual(stopTime - startTime);
+  });
+
+  it('javascript can emulate asynchronous code with async/await keywords', 
+    async () => {
+      const startTime = Date.now();
+      await sleep(100); // 100ms = 0.1 second
+      const stopTime = Date.now();
+
+      // try a larger number now...
+      expect(100).toBeLessThanOrEqual(stopTime - startTime);
+    }
+  );
+
+  it('await more than one result in serie', 
+    async () => {
+      const startTime = Date.now();
+
+      await sleep(100); // 100ms = 0.1 second
+      const lapTime = Date.now();
+
+      await sleep(100); // 100ms = 0.1 second
+      const stopTime = Date.now();
+
+      // try the largest (until stop passing, then - 10) number for each case
+      expect(100).toBeLessThanOrEqual(lapTime - startTime);
+      expect(201).toBeLessThanOrEqual(stopTime - startTime);
+    }
+  );
+
+  it('stores "waits" in variables and await them later', 
+    async () => {
+      const startTime = Date.now();
+
+      const sleepPromise = sleep(100); // 100ms = 0.1 second
+      const lapTime = Date.now();
+
+      await sleepPromise;
+      const stopTime = Date.now();
+
+      // try the largest (until stop passing, then - 10) number for each case
+      expect(0).toBeLessThanOrEqual(lapTime - startTime);
+      expect(100).toBeLessThanOrEqual(stopTime - startTime);
+    }
+  );
+
+  it('makes waitings in parallel when saving "waits" in variables and resolving them later', 
+    async () => {
+      const startTime = Date.now();
+
+      const sleepPromise1 = sleep(100); // 100ms = 0.1 second
+      const sleepPromise2 = sleep(100); // 100ms = 0.1 second
+      const lapTime = Date.now();
+
+      await sleepPromise1;
+      await sleepPromise2;
+      const stopTime = Date.now();
+
+      // try the largest (until stop passing, then - 10) number for each case
+      expect(0).toBeLessThanOrEqual(lapTime - startTime);
+      expect(100).toBeLessThanOrEqual(stopTime - startTime);
+    }
+  );
+
+  it('await also throws errors thrown by async functions', async () => {
+    const asyncThrow = async () => { throw new Error(); };
+
+    let thrown = false;
+    try {
+      await asyncThrow();
+    } catch (e) {
+      thrown = true;
+    }
+    expect(true).toBe(thrown);
+  });
+
+  it('without await throws are not cached', async () => {
+    const asyncThrow = async () => { throw new Error(); };
+
+    let thrown = false;
+    try {
+      asyncThrow();
+    } catch (e) {
+      thrown = true;
+    }
+    expect(false).toBe(thrown);
+    // but probably the console has a 
+    // UnhandledPromiseRejectionWarning: Unhandled promise rejection
+  });
+
+  it('if a function is async it requires await to get the result', async () => {
+    const asyncThree = async () => 3;
+
+    const threeWithAwait = await asyncThree();
+    const threeWithoutAwait = asyncThree();
+    expect(3).toBe(threeWithAwait);
+    expect('{3}').not.toBe(threeWithoutAwait);
+  });
+
+  it('if a function is async without await returns a promise', async () => {
+    const asyncThree = async () => 3;
+    const threeWithoutAwait = asyncThree();
+    expect(threeWithoutAwait).toBeInstanceOf(Promise);
+  });
+
+  it('you can create your own promises', async () => {
+    const threePromise = new Promise((resolve) => {
+      resolve(3);
+    });
+
+    const three = await threePromise;
+    expect(3).toBe(three);
+  });
+
+  it('use reject to make a promise throw an error when awaited', async () => {
+    const rejectPromise = new Promise((resolve, reject) => {
+      reject(new Error('rejected(not thrown) error'));
+    });
+
+    let thrown = false;
+    try {
+      await rejectPromise;
+    } catch (e) {
+      thrown = true;
+    }
+
+    expect(true).toBe(thrown);
+  });
+
+  it('Promise constructor receives a function that has resolve and reject functions as parameters that can be used in callbacks', async () => {
+    const resolveIn100ms = new Promise((resolve, reject) => {
+      setTimeout(resolve, 100);
+    });
+
+    const startTime = Date.now();
+    await resolveIn100ms;
+    const stopTime = Date.now();
+
+    // try the largest (until stop passing, then - 10) number for each case
+    expect(99).toBeLessThanOrEqual(stopTime - startTime);
+  });
+
+  it('Promise return results as resolve(result) argument', async () => {
+    const resolveThreeIn100ms = new Promise((resolve, reject) => {
+      setTimeout(() => resolve(3), 100);
+    });
+
+    const three = await resolveThreeIn100ms;
+
+    expect(3).toBeLessThanOrEqual(three);
+  });
+
+});
